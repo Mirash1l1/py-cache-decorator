ada2b17046e3f636c4e335d546339b03f6ace971
Mirash1l1 <killer28392@gmail.com>
2026-04-23 21:16:41 +0300

    Задание сделано и поменяно как попросили 2.0

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cash = {}
+    cached_results = {}
 
     def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
 
         if key in cash:
             print("Getting from cache")
-            return cash[key]
+            return cached_results[key]
 
         print("Calculating new result")
         result = func(*args, **kwargs)
